b3ce9214b08f9154734641c30f04a8094abf0d56
Psycadelic87 <stephan.goetz.hl@gmx.de>
2025-06-08 02:34:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,11 @@ def start_websocket():
 def index():
     config = load_config()
     if request.method == 'POST':
+        print("TESTTEST", )
+        config = load_config()
+    for key in config:
+        send_wled_command(config[key])
+            break
         for key in config.keys():
             config[key] = request.form.get(key)
         save_config(config)
